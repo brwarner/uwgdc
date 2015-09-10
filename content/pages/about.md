839bdb918cd20f1ae5f01db973c596f5cafa6c84
brwarner <brwarner2@gmail.com>
2015-09-10 19:09:58 +0000

    Initial commit

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Title: About Us
+Slug: about
+Date: 08-09-2015 19:44
+
+This is a test.
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Brook Jensen'
+SITENAME = u'University of Waterloo Game Development Club'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+THEME = 'themes/gdc'
+
+GOOGLE_CALENDER = 'https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=80ar36dhg37vskmhd4gjb2els4%40group.calendar.google.com&amp;color=%23691426&amp;ctz=America%2FToronto'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
